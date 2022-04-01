1f7e692e9b1cf54fd1d0ae47c73d2862929513cc
Cumar Cumar <umar-19@live.com>
2022-03-31 23:56:35 -0500

    pushing again

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
-var todayDate = moment().format('dddd, MMM Do YYYY');
+
+    
+    $(document).ready(function(){
+        var todayDate = moment().format('dddd, MMM Do YYYY');
     let displayDate = document.getElementById("currentDay").innerHTML = todayDate;
    
-var saveBtn = document.getElementById(saveBtn)
-    addEventListener(onclick , saveBtn)
-    console.log("the button has been pressed!")
+
+    $(".saveBtn").on("click",function(){
+        //console.log("the button has been pressed!")
+        var userText = $(this).siblings(".description").val()
+        var key = $(this).parent().attr("id")
+        localStorage.setItem(key , userText);
+
+    })
+
+    $("#hour9 .description").val(localStorage.getItem("hour9"))
+    $("#hour10 .description").val(localStorage.getItem("hour10"))
+    $("#hour11 .description").val(localStorage.getItem("hour11"))
+    $("#hour12 .description").val(localStorage.getItem("hour12"))
+    $("#hour13 .description").val(localStorage.getItem("hour13"))
+    $("#hour14 .description").val(localStorage.getItem("hour14"))
+    $("#hour15 .description").val(localStorage.getItem("hour15"))
+    $("#hour16 .description").val(localStorage.getItem("hour16"))
+    $("#hour17 .description").val(localStorage.getItem("hour17"))
+
+
+    var currentTime = moment().hours()
+        //console.log(currentTime)
+    
+    function timeTracker() {
+        $(".time-block").each(function(){
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1])
+            //console.log(timeBlock)
+            $(this).removeClass("past present future")
+            if (timeBlock < currentTime){
+                $(this).addClass("past")
+                //$(this).removeClass("time-block")
+            }
+            else if (timeBlock == currentTime){
+                $(this).addClass("present")
+                //$(this).removeClass("time-block")
+            }
+            else if (timeBlock > currentTime){
+                $(this).addClass("future")
+                //$(this).removeClass("time-block")
+            }
+        })
+    }
+
+    timeTracker()
+    })
+
+
+    
+    
